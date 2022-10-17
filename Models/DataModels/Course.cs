61238ddbda500c79d3b3734e79d4d3bb2eec89db
Axel Romero <axelromero@alu.frp.utn.edu.ar>
2022-10-17 11:03:54 -0300

    Correccion ejer. 1 y carga del ejer. 2

---
+++ Models/DataModels/Course.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course: BaseEntity
+    {
+        public enum Level
+        {
+            Basic,
+            Medium,
+            Advance,
+            Expert
+        }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        public string TargetAudience { get; set; } = string.Empty;
+        public string Objectives { get; set; } = string.Empty;
+        public string Requirements { get; set; } = string.Empty;
+        public Level level { get; set; } = Level.Basic;
+
+        [Required]
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+
+        [Required]
+        public Chapter Chapter { get; set; } = new Chapter();
+
+        [Required]
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+}
